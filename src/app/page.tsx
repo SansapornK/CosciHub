5f13c4c43aead634b307c65cc93efb63ff0af3a8
gimmetorkor <98889343+gimmetorkor@users.noreply.github.com>
2026-04-21 23:59:19 +0700

    [Home] edit layout, error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -627,9 +627,9 @@ const ABOUT_FEATURES = [
         viewBox="0 0 24 24"
         fill="currentColor"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className="lucide lucide-star-icon lucide-star text-white"
       >
         <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
@@ -644,9 +644,9 @@ const ABOUT_FEATURES = [
         viewBox="0 0 24 24"
         className="h-20 w-20 text-white fill-current"
         stroke="currentColor"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path d="M12 3L1 9l11 6l9-4.91V17h2V9L12 3zM5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82z" />
       </svg>
